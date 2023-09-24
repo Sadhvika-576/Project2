da191e8fcf34249a6664c9eee817f7434d0d5a7a
Sadhvika-576 <145890382+Sadhvika-576@users.noreply.github.com>
2023-09-24 11:18:56 +0530

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -1,18 +1,8 @@
-## This first line will likely take a few seconds. Be patient!
-if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
-}
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
-}
-
-# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
-# Use the base plotting system to make a plot answering this question.
-
-subsetNEI  <- NEI[NEI$fips=="24510", ]
-
-aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
-
-png('plot2.png')
-barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
-dev.off()
+data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data1 <- subset(data, Date %in% c("1/2/2007","2/2/2007"))
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(data1$Date), data1$Time)
+data1$Datetime <- as.POSIXct(datetime)
+with(data1, {
+  plot(Global_active_power~Datetime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
+})
